d6cd4b5fa0c456857423c53e8454b896afac699e
Mac Mini User <macminiuser@skipperc.local>
2016-06-04 00:20:32 -0300

    Initial Commit

---
+++ First Iphone AppTests/First_Iphone_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  First_Iphone_AppTests.swift
+//  First Iphone AppTests
+//
+//  Created by Mac Mini User on 6/4/16.
+//  Copyright © 2016 PingoSoft. All rights reserved.
+//
+
+import XCTest
+@testable import First_Iphone_App
+
+class First_Iphone_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
