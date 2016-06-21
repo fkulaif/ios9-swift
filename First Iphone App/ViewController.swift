28ddc568ab227cf68702cafb115d54887c481c44
Mac Mini User <macminiuser@Skipperc.local>
2016-06-21 14:16:47 -0300

    added files to First iPhone App

--- First Iphone App/ViewController.swift
+++ First Iphone App/ViewController.swift
@@ -9,7 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var intNumber : Int = 0
+    var doubleDecimals : Double = 0.23
+    var doubleFloat : Float = -233.3
+    var StringName : String = "PingoSoft"
+    var isBoolean : Bool = false
 
+    @IBOutlet weak var txtNumber: UITextField!
+    @IBOutlet weak var lblLabel: UILabel!
+    @IBOutlet weak var btnButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,15 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func btnButtonACTION(sender: UIButton) {
+        intNumber = Int(txtNumber.text!)!
+        changeText()
+    }
 
-
+    func changeText(){
+        lblLabel.text = "\(intNumber)"
+        btnButton.setTitle("\(intNumber)", forState: UIControlState.Normal)
+    }
 }
 
